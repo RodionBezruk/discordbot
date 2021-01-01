90c609b41eba6975f988ec76592f3509f982f1db
Rodion <rodion.bezruk@devit-team.com>
2021-01-01 11:26:22 +0000

    Corrected URL formatting.

--- triggers/github.js
+++ triggers/github.js
@@ -9,6 +9,7 @@ exports.execute = function(message) {
   let match = regex.exec(message.content);
   if (match) {
     let url = `https:
+    logger.info(url);
     request(url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         message.channel.sendMessage(`Github Pull Request: ${url}`);
