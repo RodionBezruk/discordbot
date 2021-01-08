38f5315399ef31faac8789ecc2b49bbc6f1f68a2
Rodion <rodion.bezruk@devit-team.com>
2021-01-08 13:57:46 +0000

    Fixed count.forEach being undefined. Presented a message when the user had no warnings.

--- commands/clearWarnings.js
+++ commands/clearWarnings.js
@@ -4,10 +4,14 @@ var logger = require('../logging.js');
 exports.roles = ['Admins', 'Moderators'];
 exports.command = function(message) {
   message.mentions.users.map((user) => {
-    var count = app.warnings.filter(x => x.id == user.id && !x.cleared).length || 0;
-    count.forEach(warning => warning.cleared = true);
-    data.flushWarnings();
-    message.channel.sendMessage(`${user}, your warnings have been cleared.`);
+    var count = app.warnings.filter(x => x.id == user.id && !x.cleared).length;
+    if (count != null && count.length > 0) {
+      count.forEach(warning => warning.cleared = true);
+      data.flushWarnings();
+      message.channel.sendMessage(`${user}, your warnings have been cleared.`);
+    } else {
+      message.channel.sendMessage(`${user}, you have no warnings to clear.`);
+    }
     logger.info(`${message.author.toString()} has cleared all warnings for ${user.toString()} [${count}].`);
     app.logChannel.sendMessage(`${message.author.toString()} has cleared all warnings for ${user.toString()} [${count}].`);
   });
