2e97408425abf981d67cd608ff349baed90a3b9e
Rodion <rodion.bezruk@devit-team.com>
2021-01-11 22:42:21 +0000

    Fixed list of warnings returning length instead of the actual list.

--- commands/clearWarnings.js
+++ commands/clearWarnings.js
@@ -4,7 +4,7 @@ var logger = require('../logging.js');
 exports.roles = ['Admins', 'Moderators'];
 exports.command = function(message) {
   message.mentions.users.map((user) => {
-    var count = app.warnings.filter(x => x.id == user.id && !x.cleared).length;
+    var count = app.warnings.filter(x => x.id == user.id && !x.cleared);
     if (count != null && count.length > 0) {
       count.forEach(warning => warning.cleared = true);
       data.flushWarnings();
