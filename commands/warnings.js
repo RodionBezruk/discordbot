5dc0f4a0309f16edaf093f95a8cec55809824a4d
Rodion <rodion.bezruk@devit-team.com>
2021-01-14 19:39:36 +0000

    Updated variable name for wearnings.

--- commands/warnings.js
+++ commands/warnings.js
@@ -2,7 +2,7 @@ var app = require('../app.js');
 var logger = require('../logging.js');
 exports.command = function(message) {
   message.mentions.users.map((user) => {
-    var count = app.warnings.filter(x => x.id == user.id && !x.cleared).length || 0;
-    message.channel.sendMessage(`${user}, you have ${count} warnings.`);
+    var warnings = app.warnings.filter(x => x.id == user.id && !x.cleared);
+    message.channel.sendMessage(`${user}, you have ${count.warnings} total warnings.`);
   });
 }
