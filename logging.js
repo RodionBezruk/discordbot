689c67f669425dcfc5202077e24d083ec4a6b39c
Rodion <rodion.bezruk@devit-team.com>
2021-01-27 03:09:43 +0000

    Added logdna.

--- logging.js
+++ logging.js
@@ -2,23 +2,25 @@ var winston = require('winston');
 winston.emitErrs = true;
 var logger = new winston.Logger({
     transports: [
-        new winston.transports.File({
-            level: 'info',
-            filename: './server.log',
-            handleExceptions: true,
-            humanReadableUnhandledException: true,
-            json: true,
-            maxsize: 5242880, 
-            maxFiles: 5,
-            colorize: false
-        }),
         new winston.transports.Console({
-            level: 'debug',
-            handleExceptions: true,
-            json: false,
-            colorize: true
+          level: 'silly',
+          handleExceptions: true,
+          humanReadableUnhandledException: true,
+          json: false,
+          colorize: true
         })
     ],
+    handleExceptions: true,
     exitOnError: false
 });
+if (process.env.NODE_ENV == 'production') {
+    logger.add(winston.transports.Logdna, {
+        level: 'silly',
+        key: config.logdnaKey,
+        ip: ip.address(),
+        hostname: os.hostname(),
+        app: 'services-discordbot'
+    });
+    logger.info('[logging] Started LogDNA winston transport.');
+}
 module.exports = logger;
